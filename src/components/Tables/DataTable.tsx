c275a1fd24ee383ff6e9553064ac392b6ec72a81
vbellemin <victorbellemin@outlook.fr>
2023-06-07 13:02:10 -0400

    The layout of the dataTable is ok

--- src/components/Tables/DataTable.tsx
+++ src/components/Tables/DataTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import {
   Table,
   TableWrapper,
@@ -17,7 +17,6 @@ interface Props {
 
 const DataTable: React.FC<Props> = ({
   tableHead = [
-    "",
     "0",
     "1",
     "2",
@@ -37,30 +36,39 @@ const DataTable: React.FC<Props> = ({
 }) => {
   return (
     <View style={styles.container}>
-      <Table borderStyle={{ borderWidth: 1 }}>
-        <Row 
-          data={tableHead} 
-          style={styles.head} 
-          textStyle={styles.text} 
-          widthArr={[100]}
-          />
-        <TableWrapper style={styles.wrapper}>
+      <View style={{ width: 100}}>
+        <Table borderStyle={{ borderWidth: 1 }}>
           <Col 
-            data={tableTitle} 
+            data={["", ...tableTitle]} 
             style={styles.title} 
             width={100}
-            heightArr={[40, 60, 40, 60, 40]}
+            heightArr={[40, 40, 60, 40, 60, 40]}
             textStyle={styles.text} 
             />
-          <Rows
-            data={tableData ? tableData : [[]]}
-            heightArr={[40, 60, 40, 60, 40]}
-            flexArr={[1, 1, 1, 1, 1, 1, 1 , 1, 1, 1, 1, 1, 1]}
-            style={styles.row}
-            textStyle={styles.text}
-          />
-        </TableWrapper>
-      </Table>
+        </Table>
+      </View>
+      <ScrollView horizontal={true}>
+        <View style={{ width: 800}}>
+          <Table borderStyle={{ borderWidth: 1 }}>
+              <TableWrapper style={styles.wrapper_rows}>
+                <Row 
+                data={tableHead} 
+                style={styles.head} 
+                flexArr={[1, 1, 1, 1, 1, 1, 1 , 1, 1, 1, 1, 1, 1]}
+                textStyle={styles.text} 
+                widthArr={[100]}
+                />
+                <Rows
+                  data={tableData ? tableData : [[]]}
+                  heightArr={[40, 60, 40, 60, 40]}
+                  flexArr={[1, 1, 1, 1, 1, 1, 1 , 1, 1, 1, 1, 1, 1]}
+                  style={styles.row}
+                  textStyle={styles.text}
+                />
+              </TableWrapper>
+          </Table>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -68,13 +76,17 @@ const DataTable: React.FC<Props> = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     padding: 16,
     paddingTop: 30,
     backgroundColor: "#fff",
     width: "100%",
+    alignSelf: "center",
   },
   head: { height: 40, backgroundColor: "#f1f8ff", width: "100%" },
   wrapper: { flex: 1, flexDirection: "row" , width: "100%"},
+  wrapper_rows: { flex: 1, flexDirection: "column" , width: "100%"},
+  dataWrapper: { },
   title: { backgroundColor: "#f6f8fa"},
   row: {flex :1, width: "100%"},
   text: { textAlign: "center" },
